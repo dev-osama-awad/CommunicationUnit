9184d0a5a5146c08014900d53b3268eef57dd588
Osama Awad <osamajawad.95@hotmail.com>
2023-04-01 11:31:47 +0200

    init commit

---
+++ src/main/kotlin/com/terminy/CommunicationUnit/CommunicationUnitApplication.kt
@@ -0,0 +1,11 @@
+package com.terminy.CommunicationUnit
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CommunicationUnitApplication
+
+fun main(args: Array<String>) {
+	runApplication<CommunicationUnitApplication>(*args)
+}
---
+++ src/test/kotlin/com/terminy/CommunicationUnit/CommunicationUnitApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.terminy.CommunicationUnit
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CommunicationUnitApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
